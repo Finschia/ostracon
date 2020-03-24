6a37c4a36c00d670333ec3391bc49f9abff856dd
hongsup.so <hongsup.so@linecorp.com>
2020-03-24 10:33:45 +0900

    Change golangci-lint version

--- scripts/current_branch_lint.sh
+++ scripts/current_branch_lint.sh
@@ -7,8 +7,8 @@ base_commit=$(diff -u <(git rev-list --first-parent HEAD) \
 
 if [ "$base_commit" ]; then
 	#if base commit of this branch is exist, run linter from base commit to HEAD
-	golangci-lint run -v --new-from-rev "$base_commit"
+	golangci-lint run --new-from-rev "$base_commit"
 else
 	# if not, check current don't commited files.
-	golangci-lint run -n -v
+	golangci-lint run -n
 fi
