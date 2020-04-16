5be98dff6a07626d6943d14af3a579843078c77a
zemyblue <zemyblue@gmail.com>
2020-04-16 22:28:26 +0900

    Fix skipped `TestStateProposerSelection0`

--- consensus/state_test.go
+++ consensus/state_test.go
@@ -52,9 +52,6 @@ x * TestHalt1 - if we see +2/3 precommits after timing out into new round, we sh
 // ProposeSuite
 
 func TestStateProposerSelection0(t *testing.T) {
-	// FIXME
-	t.Skip("Temporarily excluded because this a case that doesn't end due to Proposer selection changes.")
-
 	cs1, vss := randState(4)
 	height, round := cs1.Height, cs1.Round
 
@@ -83,7 +80,7 @@ func TestStateProposerSelection0(t *testing.T) {
 	ensureNewRound(newRoundCh, height+1, 0)
 
 	prop = cs1.GetRoundState().Proposer
-	addr := vss[1].GetPubKey().Address()
+	addr := types.SelectProposer(cs1.Validators, cs1.state.LastProofHash, cs1.Height, cs1.Round).PubKey.Address()
 	if !bytes.Equal(prop.Address, addr) {
 		panic(fmt.Sprintf("expected proposer to be validator %d. Got %X", 1, prop.Address))
 	}
