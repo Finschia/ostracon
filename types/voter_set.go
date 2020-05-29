6e9df552354aad76171595060632d7c9e7a58e16
zemyblue <zemyblue@gmail.com>
2020-05-29 20:42:58 +0900

    fix lint error

--- types/voter_set.go
+++ types/voter_set.go
@@ -431,7 +431,7 @@ func SelectVoter(validators *ValidatorSet, proofHash []byte) *VoterSet {
 	voters := 0
 	for _, candi := range candidates {
 		if candi.(*candidate).win > 0 {
-			voters += 1
+			voters++
 		}
 	}
 
