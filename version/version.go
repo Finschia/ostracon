042125ea9c71a42b76b8917e2adffa606bb1dbd9
zemyblue <zemyblue@gmail.com>
2020-06-22 20:26:30 +0900

    Change LINE Core version to 0.2

--- version/version.go
+++ version/version.go
@@ -20,7 +20,7 @@ func init() {
 
 const (
 	// LINECoreSemVer is the current version of LINE Tendermint Core.
-	LINECoreSemVer = "0.1"
+	LINECoreSemVer = "0.2"
 
 	// ABCISemVer is the semantic version of the ABCI library
 	ABCISemVer = "0.17.0"
