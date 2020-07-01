c0424f128d0092423e2549e0d74810957820c128
Woosang Son <woosang.son@linecorp.com>
2020-07-01 17:48:24 +0900

    test: add TestVerifyAdjacentHeadersWithVoterSampling

--- light/verifier_test.go
+++ light/verifier_test.go
@@ -189,6 +189,179 @@ func TestVerifyAdjacentHeaders(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestVerifyAdjacentHeadersWithVoterSampling(t *testing.T) {
+	const (
+		chainID    = "TestVerifyAdjacentHeaders"
+		lastHeight = 1
+		nextHeight = 2
+	)
+
+	var (
+		voterParamsHalf = &types.VoterParams{
+			VoterElectionThreshold:          5,
+			MaxTolerableByzantinePercentage: 10,
+			ElectionPrecision:               3,
+		}
+		keys = genPrivKeys(10)
+		// 100, 110, ..., 200
+		vals     = keys.ToValidators(100, 10)
+		bTime, _ = time.Parse(time.RFC3339, "2006-01-02T15:04:05Z")
+		header   = keys.GenSignedHeader(chainID, lastHeight, bTime, nil,
+			vals, vals,
+			[]byte("app_hash"), []byte("cons_hash"), []byte("results_hash"), 0, len(keys), voterParamsHalf)
+	)
+
+	otherVals := keys.ToValidators(200, 1)
+
+	testCases := []struct {
+		newHeader      *types.SignedHeader
+		newVals        *types.ValidatorSet
+		trustingPeriod time.Duration
+		now            time.Time
+		expErr         error
+		expErrText     string
+	}{
+		// same header -> no error
+		0: {
+			header,
+			vals,
+			3 * time.Hour,
+			bTime.Add(2 * time.Hour),
+			nil,
+			"headers must be adjacent in height",
+		},
+		// different chainID -> error
+		1: {
+			keys.GenSignedHeader("different-chainID", nextHeight, bTime.Add(1*time.Hour), nil,
+				vals, vals,	[]byte("app_hash"), []byte("cons_hash"), []byte("results_hash"), 0, len(keys),
+				voterParamsHalf),
+			vals,
+			3 * time.Hour,
+			bTime.Add(2 * time.Hour),
+			nil,
+			"untrustedHeader.ValidateBasic failed: signedHeader belongs to another chain 'different-chainID' not" +
+				" 'TestVerifyAdjacentHeaders'",
+		},
+		// new header's time is before old header's time -> error
+		2: {
+			keys.GenSignedHeader(chainID, nextHeight, bTime.Add(-1*time.Hour), nil,
+				vals, vals, []byte("app_hash"), []byte("cons_hash"), []byte("results_hash"), 0, len(keys),
+				voterParamsHalf),
+			vals,
+			3 * time.Hour,
+			bTime.Add(2 * time.Hour),
+			nil,
+			"to be after old header time",
+		},
+		// new header's time is from the future -> error
+		3: {
+			keys.GenSignedHeader(chainID, nextHeight, bTime.Add(3*time.Hour), nil,
+				vals, vals,	[]byte("app_hash"), []byte("cons_hash"), []byte("results_hash"), 0, len(keys),
+				voterParamsHalf),
+			vals,
+			3 * time.Hour,
+			bTime.Add(2 * time.Hour),
+			nil,
+			"new header has a time from the future",
+		},
+		// new header's time is from the future, but it's acceptable (< maxClockDrift) -> no error
+		4: {
+			keys.GenSignedHeader(chainID, nextHeight,
+				bTime.Add(2*time.Hour).Add(maxClockDrift).Add(-1*time.Millisecond), nil,
+				vals, vals, []byte("app_hash"), []byte("cons_hash"), []byte("results_hash"), 0, len(keys),
+				voterParamsHalf),
+			vals,
+			3 * time.Hour,
+			bTime.Add(2 * time.Hour),
+			nil,
+			"",
+		},
+		// 3/3 signed -> no error
+		5: {
+			keys.GenSignedHeaderByRate(chainID, nextHeight, bTime.Add(1*time.Hour), nil,
+				vals, vals,	[]byte("app_hash"), []byte("cons_hash"), []byte("results_hash"), 1.0,
+				voterParamsHalf),
+			vals,
+			3 * time.Hour,
+			bTime.Add(2 * time.Hour),
+			nil,
+			"",
+		},
+		// 2/3 signed -> no error
+		6: {
+			keys.GenSignedHeaderByRate(chainID, nextHeight, bTime.Add(1*time.Hour), nil,
+				vals, vals,	[]byte("app_hash"), []byte("cons_hash"), []byte("results_hash"), 0.66,
+				voterParamsHalf),
+			vals,
+			3 * time.Hour,
+			bTime.Add(2 * time.Hour),
+			nil,
+			"",
+		},
+		// 1/3 signed -> error
+		7: {
+			keys.GenSignedHeaderByRate(chainID, nextHeight, bTime.Add(1*time.Hour), nil,
+				vals, vals,
+				[]byte("app_hash"), []byte("cons_hash"), []byte("results_hash"), 0.33,
+				voterParamsHalf),
+			vals,
+			3 * time.Hour,
+			bTime.Add(2 * time.Hour),
+			nil,
+			"invalid commit -- insufficient voting power",
+		},
+		// vals does not match with what we have -> error
+		8: {
+			keys.GenSignedHeader(chainID, nextHeight, bTime.Add(1*time.Hour), nil,
+				otherVals, vals, []byte("app_hash"), []byte("cons_hash"),
+				[]byte("results_hash"), 0, len(keys), voterParamsHalf),
+			otherVals,
+			3 * time.Hour,
+			bTime.Add(2 * time.Hour),
+			nil,
+			"to match those from new header",
+		},
+		// vals are inconsistent with newHeader -> error
+		9: {
+			keys.GenSignedHeader(chainID, nextHeight, bTime.Add(1*time.Hour), nil,
+				vals, vals,	[]byte("app_hash"), []byte("cons_hash"), []byte("results_hash"), 0, len(keys),
+				voterParamsHalf),
+			keys.ToValidators(10, 1),
+			3 * time.Hour,
+			bTime.Add(2 * time.Hour),
+			nil,
+			"to match those that were supplied",
+		},
+		// old header has expired -> error
+		10: {
+			keys.GenSignedHeader(chainID, nextHeight, bTime.Add(1*time.Hour), nil,
+				vals, vals,	[]byte("app_hash"), []byte("cons_hash"), []byte("results_hash"), 0, len(keys),
+				voterParamsHalf),
+			keys.ToValidators(10, 1),
+			1 * time.Hour,
+			bTime.Add(1 * time.Hour),
+			nil,
+			"old header has expired",
+		},
+	}
+
+	for i, tc := range testCases {
+		tc := tc
+		t.Run(fmt.Sprintf("#%d", i), func(t *testing.T) {
+			err := lite.VerifyAdjacent(chainID, header, tc.newHeader, tc.newVals, tc.trustingPeriod, tc.now,
+				maxClockDrift, voterParamsHalf)
+			switch {
+			case tc.expErr != nil && assert.Error(t, err):
+				assert.Equal(t, tc.expErr, err)
+			case tc.expErrText != "":
+				assert.Contains(t, err.Error(), tc.expErrText)
+			default:
+				assert.NoError(t, err)
+			}
+		})
+	}
 
 }
 
