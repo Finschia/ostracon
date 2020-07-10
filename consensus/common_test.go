92cd4c685a2c2b487c4f9d799a41ac78addfd2f8
Woosang Son <woosang.son@linecorp.com>
2020-07-10 17:57:03 +0900

    test: add test case; non-voter votes

--- consensus/common_test.go
+++ consensus/common_test.go
@@ -222,6 +222,15 @@ func signAddVotes(
 	addVotes(to, votes...)
 }
 
+func getValidatorBeingNotVoter(cs *State) *types.Validator {
+	for _, val := range cs.Validators.Validators {
+		if !cs.Voters.HasAddress(val.Address) {
+			return val
+		}
+	}
+	return nil
+}
+
 func validatePrevote(t *testing.T, cs *State, round int, privVal *validatorStub, blockHash []byte) {
 	prevotes := cs.Votes.Prevotes(round)
 	pubKey, err := privVal.GetPubKey()
