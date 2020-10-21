be3651ae3528bc3e7408dbce924ebf99e0f0403f
kukugi <wonkuk_seo@linecorp.com>
2020-10-21 16:55:15 +0900

    test: Write a test case to verify a new voter sampling

--- types/voter_set.go
+++ types/voter_set.go
@@ -784,7 +784,11 @@ func sortVoters(candidates []*voter) []*voter {
 	sort.Slice(temp, func(i, j int) bool {
 		bigA := new(big.Int).Mul(big.NewInt(temp[i].val.VotingPower), big.NewInt(temp[j].val.StakingPower))
 		bigB := new(big.Int).Mul(big.NewInt(temp[j].val.VotingPower), big.NewInt(temp[i].val.StakingPower))
-		return bigA.Cmp(bigB) == 1
+		compareResult := bigA.Cmp(bigB)
+		if compareResult == 0 {
+			return bytes.Compare(temp[i].val.Address, temp[j].val.Address) == -1
+		}
+		return compareResult == 1
 	})
 	return temp
 }
