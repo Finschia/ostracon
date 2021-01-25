bc7ff22f36536ec0119d6583dddbd635a426b095
KIm, JinSan <jinsan@linecorp.com>
2021-01-25 11:36:33 +0900

    feat: impl `checkTxAsyncReactor()` (#168)
    * feat: impl `checkTxAsyncReactor()`
    
    * chore: revise comment
    
    * chore: divide `cb` into `prepareCb` and `checkTxCb`
    
    * chore: revise `mock/mempool.CheckTxAsync()` to do nothing
    
    * fix: check whether `prepareCb` is `nil`

--- mempool/reactor.go
+++ mempool/reactor.go
@@ -174,10 +174,11 @@ func (memR *Reactor) Receive(chID byte, src p2p.Peer, msgBytes []byte) {
 		if src != nil {
 			txInfo.SenderP2PID = src.ID()
 		}
-		err := memR.mempool.CheckTxAsync(msg.Tx, txInfo, nil)
-		if err != nil {
-			memR.Logger.Info("Could not check tx", "tx", txID(msg.Tx), "err", err)
-		}
+		memR.mempool.CheckTxAsync(msg.Tx, txInfo, func(err error) {
+			if err != nil {
+				memR.Logger.Info("Could not check tx", "tx", txID(msg.Tx), "err", err)
+			}
+		}, nil)
 		// broadcasting happens from go routines per peer
 	default:
 		memR.Logger.Error(fmt.Sprintf("Unknown message type %v", reflect.TypeOf(msg)))
