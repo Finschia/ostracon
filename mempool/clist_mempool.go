3e848824b431ef30d4399caea3c47f483a3658c0
Kim, JinSan <jinsan@linecorp.com>
2021-04-19 11:04:59 +0900

    fix: revise to call Begin/EndRecheck even though `mem.Size()` is 0

--- mempool/clist_mempool.go
+++ mempool/clist_mempool.go
@@ -20,7 +20,6 @@ import (
 	"github.com/line/ostracon/p2p"
 	"github.com/line/ostracon/proxy"
 	"github.com/line/ostracon/types"
-	"github.com/pkg/errors"
 )
 
 // TxKeySize is the size of the transaction key index
@@ -586,7 +585,7 @@ func (mem *CListMempool) Update(
 	block *types.Block,
 	deliverTxResponses []*abci.ResponseDeliverTx,
 	preCheck PreCheckFunc,
-) error {
+) (err error) {
 	// Set height
 	mem.height = block.Height
 	mem.notifiedTxsAvailable = false
@@ -619,49 +618,44 @@ func (mem *CListMempool) Update(
 		}
 	}
 
-	// Either recheck non-committed txs to see if they became invalid
-	// or just notify there're some txs left.
-	recheckStartTime := time.Now().UnixNano()
-	if mem.Size() > 0 {
-		if mem.config.Recheck {
-			mem.logger.Debug("recheck txs", "numtxs", mem.Size(), "height", block.Height)
-			res, err := mem.proxyAppConn.BeginRecheckTxSync(abci.RequestBeginRecheckTx{
-				Header: types.OST2PB.Header(&block.Header),
-			})
-			if res.Code == abci.CodeTypeOK && err == nil {
-				mem.recheckTxs()
-				res2, err2 := mem.proxyAppConn.EndRecheckTxSync(abci.RequestEndRecheckTx{Height: block.Height})
-				if res2.Code != abci.CodeTypeOK {
-					return errors.New("the function EndRecheckTxSync does not respond CodeTypeOK")
-				}
-				if err2 != nil {
-					return errors.Wrap(err2, "the function EndRecheckTxSync returns an error")
-				}
-			} else {
-				if res.Code != abci.CodeTypeOK {
-					return errors.New("the function BeginRecheckTxSync does not respond CodeTypeOK")
-				}
-				if err != nil {
-					return errors.Wrap(err, "the function BeginRecheckTxSync returns an error")
-				}
-			}
+	if mem.config.Recheck {
+		// recheck non-committed txs to see if they became invalid
+		recheckStartTime := time.Now().UnixNano()
 
-			// At this point, mem.txs are being rechecked.
-			// mem.recheckCursor re-scans mem.txs and possibly removes some txs.
-			// Before mem.Reap(), we should wait for mem.recheckCursor to be nil.
-		} else {
+		_, err = mem.proxyAppConn.BeginRecheckTxSync(abci.RequestBeginRecheckTx{
+			Header: types.OST2PB.Header(&block.Header),
+		})
+		if err != nil {
+			mem.logger.Error("error in proxyAppConn.BeginRecheckTxSync", "err", err)
+		}
+
+		mem.logger.Info("recheck txs", "numtxs", mem.Size(), "height", block.Height)
+		mem.recheckTxs()
+
+		_, err = mem.proxyAppConn.EndRecheckTxSync(abci.RequestEndRecheckTx{Height: block.Height})
+		if err != nil {
+			mem.logger.Error("error in proxyAppConn.EndRecheckTxSync", "err", err)
+		}
+
+		recheckEndTime := time.Now().UnixNano()
+
+		recheckTimeMs := float64(recheckEndTime-recheckStartTime) / 1000000
+		mem.metrics.RecheckTime.Set(recheckTimeMs)
+
+		// At this point, mem.txs are being rechecked.
+		// mem.recheckCursor re-scans mem.txs and possibly removes some txs.
+		// Before mem.Reap(), we should wait for mem.recheckCursor to be nil.
+	} else {
+		// just notify there're some txs left.
+		if mem.Size() > 0 {
 			mem.notifyTxsAvailable()
 		}
 	}
-	recheckEndTime := time.Now().UnixNano()
-
-	recheckTimeMs := float64(recheckEndTime-recheckStartTime) / 1000000
-	mem.metrics.RecheckTime.Set(recheckTimeMs)
 
 	// Update metrics
 	mem.metrics.Size.Set(float64(mem.Size()))
 
-	return nil
+	return err
 }
 
 func (mem *CListMempool) recheckTxs() {
