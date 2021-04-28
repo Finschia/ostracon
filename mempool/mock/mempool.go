4750c7be761683597384abcf7bc45c38b2fd1f41
KIm, JinSan <jinsan@linecorp.com>
2021-04-28 14:54:37 +0900

    feat: impl checkTxAsyncReactor() (#168) (#225)
    * feat: impl checkTxAsyncReactor() (#168)
    
    * fix: tests
    
    * fix: lint errors

--- mempool/mock/mempool.go
+++ mempool/mock/mempool.go
@@ -18,8 +18,7 @@ func (Mempool) Size() int { return 0 }
 func (Mempool) CheckTxSync(_ types.Tx, _ mempl.TxInfo) (*abci.Response, error) {
 	return nil, nil
 }
-func (Mempool) CheckTxAsync(_ types.Tx, _ mempl.TxInfo, _ func(*abci.Response)) error {
-	return nil
+func (Mempool) CheckTxAsync(_ types.Tx, _ mempl.TxInfo, _ func(error), _ func(*abci.Response)) {
 }
 func (Mempool) ReapMaxBytesMaxGas(_, _ int64) types.Txs { return types.Txs{} }
 func (Mempool) ReapMaxTxs(n int) types.Txs              { return types.Txs{} }
