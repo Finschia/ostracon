0fbd57a3a9bb13cbc765626a087959caf271f589
tnasu <toshimasa_nasu@hotmail.com>
2021-05-28 17:43:06 +0900

    docs: Update documents to Ostracon

--- consensus/README.md
+++ consensus/README.md
@@ -1,3 +1,3 @@
-# Consensus 
+# Consensus
 
-See the [consensus spec](https://github.com/tendermint/spec/tree/master/spec/consensus) and the [reactor consensus spec](https://github.com/tendermint/spec/tree/master/spec/reactors/consensus) for more information.
+Basically, see the [Tendermint v0.34.8 Consensus spec](https://github.com/tendermint/tendermint/blob/v0.34.8/consensus/README.md).
--- scripts/linkify_changelog.py
+++ scripts/linkify_changelog.py
@@ -5,9 +5,9 @@ import re
 # with the valid mark down formatted link to it. e.g.
 # " [\#number](https://github.com/tendermint/tendermint/issues/<number>)
 # Note that if the number is for a PR, github will auto-redirect you when you click the link.
-# It is safe to run the script multiple times in succession. 
+# It is safe to run the script multiple times in succession.
 #
 # Example usage $ python3 linkify_changelog.py ../CHANGELOG_PENDING.md
 for line in fileinput.input(inplace=1):
-    line = re.sub(r"\s\\#([0-9]*)", r" [\\#\1](https://github.com/tendermint/tendermint/issues/\1)", line.rstrip())
+    line = re.sub(r"\s\\#([0-9]*)", r" [\\#\1](https://github.com/line/ostracon/issues/\1)", line.rstrip())
     print(line)
