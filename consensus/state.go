7b6e766124312e98cab8c3bf771378915fbe00a1
Sangyeop.lee <sangyeop.lee@linecorp.com>
2021-06-10 15:32:15 +0900

    perf: do not flush wal when receive consensus msgs

--- consensus/state.go
+++ consensus/state.go
@@ -816,15 +816,6 @@ func (cs *State) receiveRoutine(maxSteps int) {
 			if err := cs.wal.Write(mi); err != nil {
 				cs.Logger.Error("failed writing to WAL", "err", err)
 			}
-			go func() {
-				if err := cs.wal.FlushAndSync(); err != nil {
-					cs.Logger.Error("CONSENSUS FAILURE!!!", "err", fmt.Sprintf(
-						"failed to write %v msg to consensus WAL due to %v; check your file system and restart the node",
-						mi, err,
-					), "stack", string(debug.Stack()))
-					onExit(cs)
-				}
-			}()
 
 			if _, ok := mi.Msg.(*VoteMessage); ok {
 				// we actually want to simulate failing during
