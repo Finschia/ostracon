2f59000d56e394da5fd0899142b020d060748bf8
tnasu <toshimasa_nasu@hotmail.com>
2021-07-06 17:30:40 +0900

    (squash) Change Tendermint to Ostracon

--- rpc/core/doc.go
+++ rpc/core/doc.go
@@ -1,7 +1,7 @@
 /*
-Package core defines the Tendermint RPC endpoints.
+Package core defines the Ostracon RPC endpoints.
 
-Tendermint ships with its own JSONRPC library -
+Ostracon ships with its own JSONRPC library -
 https://github.com/line/ostracon/tree/main/rpc/jsonrpc.
 
 ## Get the list
