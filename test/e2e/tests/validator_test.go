fd45f8ec88e2ea2b351127ad67d2e634fed0ee57
tnasu <toshimasa_nasu@hotmail.com>
2021-08-31 16:54:11 +0900

    (fix) Remove t.Skip: TestValidator_Propose

--- test/e2e/tests/validator_test.go
+++ test/e2e/tests/validator_test.go
@@ -75,7 +75,7 @@ func TestValidator_Sets(t *testing.T) {
 // Tests that a validator proposes blocks when it's supposed to. It tolerates some
 // missed blocks, e.g. due to testnet perturbations.
 func TestValidator_Propose(t *testing.T) {
-	t.Skip("NON-deterministic test: SelectProposer is not well since valSchedule.Set is changed")
+	t.Skip("Ostracon doesn't select a Proposer based on ProposerPriority")
 	blocks := fetchBlockChain(t)
 	testNode(t, func(t *testing.T, node e2e.Node) {
 		if node.Mode != e2e.ModeValidator {
