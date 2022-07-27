18bdbd71ca37ce17a83b94b8367cd9390ff5a741
Torao Takami <torao.takami@linecorp.com>
2022-07-27 13:21:44 +0900

    change PubKey/PrivKey prefixes of "ostracon/" to "tendermint (#445) (#447)
    1. The private key may remain "pstracon/PrivKeyXxx" as it's never exposed to outside world. However, it makes no sense to keep the ostracon prefix even with asymmetric naming, so it should be modified with "tendermint/PrivKeyXxx"
    2. BLS12 and Composite key types are not compatible with current Cosmos assets to begin with.  To make this clear, these Ostracon-specific prefixes are left as "ostracon/".

--- crypto/sr25519/encoding.go
+++ crypto/sr25519/encoding.go
@@ -8,8 +8,8 @@ import (
 var _ crypto.PrivKey = PrivKey{}
 
 const (
-	PrivKeyName = "ostracon/PrivKeySr25519"
-	PubKeyName  = "ostracon/PubKeySr25519"
+	PrivKeyName = "tendermint/PrivKeySr25519"
+	PubKeyName  = "tendermint/PubKeySr25519"
 
 	// SignatureSize is the size of an Edwards25519 signature. Namely the size of a compressed
 	// Sr25519 point, and a field element. Both of which are 32 bytes.
