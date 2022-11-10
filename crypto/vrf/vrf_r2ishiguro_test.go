990d2aba695074da6cc47f144bf74692fad44c60
Mdaiki0730 <garian0730.develop@gmail.com>
2022-11-10 14:20:37 +0900

    Add test for r2ishiguro.ProofToHash()

--- crypto/vrf/vrf_r2ishiguro_test.go
+++ crypto/vrf/vrf_r2ishiguro_test.go
@@ -10,6 +10,31 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+func TestVrfEd25519r2ishiguro_ProofToHash(t *testing.T) {
+	secret := [SEEDBYTES]byte{}
+	privateKey := ed25519.NewKeyFromSeed(secret[:])
+	message := []byte("hello, world")
+
+	vrfr2ishiguro := newVrfEd25519r2ishiguro()
+
+	t.Run("to hash r2ishiguro proof", func(t *testing.T) {
+		proof, err := vrfr2ishiguro.Prove(privateKey, message)
+		require.NoError(t, err)
+		require.NotNil(t, proof)
+
+		output, err := vrfr2ishiguro.ProofToHash(proof)
+		require.NoError(t, err)
+		require.NotNil(t, output)
+	})
+
+	t.Run("to hash other algo proof", func(t *testing.T) {
+		proof := []byte("proof of test")
+		output, err := vrfr2ishiguro.ProofToHash(proof)
+		require.Error(t, err)
+		require.Nil(t, output)
+	})
+}
+
 func TestProveAndVerifyR2ishiguroByCryptoEd25519(t *testing.T) {
 	secret := [SEEDBYTES]byte{}
 	privateKey := ed25519.NewKeyFromSeed(secret[:])
