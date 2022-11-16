a6a45fd476f93961caa725ab220bee91d2238cd5
Shogo Hyodo <mmoshg8u@gmail.com>
2022-11-16 18:48:31 +0900

    Remove logging that never happens

--- node/node.go
+++ node/node.go
@@ -198,10 +198,7 @@ func CustomReactors(reactors map[string]p2p.Reactor) Option {
 				for _, chDesc := range reactor.GetChannels() {
 					if !ni.HasChannel(chDesc.ID) {
 						ni.Channels = append(ni.Channels, chDesc.ID)
-						err := n.transport.AddChannel(chDesc.ID)
-						if err != nil {
-							n.Logger.Debug("AddChannel failed", "err", err)
-						}
+						n.transport.AddChannel(chDesc.ID)
 					}
 				}
 				n.nodeInfo = ni
