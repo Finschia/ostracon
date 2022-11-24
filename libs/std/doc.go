87481585a4e5b916ff00c093537a27e56e269a7a
Mdaiki0730 <garian0730.develop@gmail.com>
2022-11-24 11:44:18 +0900

    Add docs for libs/std package

---
+++ libs/std/doc.go
@@ -0,0 +1,4 @@
+// The std wraps standard packages that are hard to intentionally cause errors when testing.
+// This package is ignored by codecov, so you need to check if you can ignore it when adding it.
+
+package std
