e5bb43b5a3eaedf2e22284adc34c6235dc47f545
Shogo Hyodo <mmoshg8u@gmail.com>
2022-11-29 14:14:32 +0900

    Remove `SelectProposer`

--- consensus/state.go
+++ consensus/state.go
@@ -727,7 +727,6 @@ func (cs *State) updateToState(state sm.State) {
 	cs.CommitRound = -1
 	cs.LastVoters = state.LastVoters
 	cs.TriggeredTimeoutPrecommit = false
-	cs.Proposer = validators.SelectProposer(state.LastProofHash, cs.Height, cs.Round)
 
 	cs.state = state
 
