a4154f5d2a434dd182e6ed6790754ca04fe8b081
tnasu <toshimasa_nasu@hotmail.com>
2022-12-21 20:33:58 +0900

    Fix the usage of version

--- test/e2e/runner/cleanup.go
+++ test/e2e/runner/cleanup.go
@@ -16,7 +16,7 @@ func Cleanup(testnet *e2e.Testnet) error {
 	if err != nil {
 		return err
 	}
-	err = cleanupDir(testnet.Dir)
+	err = cleanupDir(testnet.Dir, testnet.Nodes[0].Version)
 	if err != nil {
 		return err
 	}
@@ -48,7 +48,7 @@ func cleanupDocker() error {
 }
 
 // cleanupDir cleans up a testnet directory
-func cleanupDir(dir string) error {
+func cleanupDir(dir, version string) error {
 	if dir == "" {
 		return errors.New("no directory set")
 	}
@@ -70,7 +70,7 @@ func cleanupDir(dir string) error {
 		return err
 	}
 	err = execDocker("run", "--rm", "--entrypoint", "", "-v", fmt.Sprintf("%v:/network", absDir),
-		"ostracon/e2e-node", "sh", "-c", "rm -rf /network/*/")
+		fmt.Sprintf("ostracon/e2e-node:%s", version), "sh", "-c", "rm -rf /network/*/")
 	if err != nil {
 		return err
 	}
