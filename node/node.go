b588e5af6f14516954228708e7b28d7ed3170ef8
dudong2 <leesj9476lsj@gmail.com>
2023-03-08 18:46:43 +0900

    Revert "feat: decrease db block chunk size"
    This reverts commit 100d340cb0af31ff9f868300fa6ff1ebda9082ba.

--- node/node.go
+++ node/node.go
@@ -1461,7 +1461,7 @@ func saveGenesisDoc(db dbm.DB, genDoc *types.GenesisDoc) error {
 		return fmt.Errorf("failed to save genesis doc due to marshaling error: %w", err)
 	}
 
-	blockSize := 0x8000000 // 100mb
+	blockSize := 0x40000000 // 1gb
 	blocks := make([][]byte, 0)
 	for i := 0; i < len(b); i += blockSize {
 		end := i + blockSize
