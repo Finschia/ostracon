6b7f7772265a4e0e80f6d3d9ec302466afc7acbf
170210 <j170210@icloud.com>
2024-03-11 14:09:21 +0900

    fix: handle the case when the proof is empty
    Signed-off-by: 170210 <j170210@icloud.com>

--- crypto/ed25519/migration.go
+++ crypto/ed25519/migration.go
@@ -41,14 +41,15 @@ func ProofToHash(proof []byte) ([]byte, error) {
 // ValidateProof returns an error if the proof is not empty, but its
 // size != vrf.ProofSize.
 func ValidateProof(h []byte) error {
-	if len(h) > 0 {
-		proofSize := len(h)
-		if proofSize != voivrf.ProofSize && proofSize != r2vrf.ProofSize {
-			return fmt.Errorf("expected size to be %d bytes, got %d bytes",
-				voivrf.ProofSize,
-				len(h),
-			)
-		}
+	if len(h) == 0 {
+		return fmt.Errorf("proof is empty")
+	}
+	proofSize := len(h)
+	if proofSize != voivrf.ProofSize && proofSize != r2vrf.ProofSize {
+		return fmt.Errorf("expected size to be %d bytes, got %d bytes",
+			voivrf.ProofSize,
+			len(h),
+		)
 	}
 	return nil
 }
